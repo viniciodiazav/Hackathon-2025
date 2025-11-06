69e6b638affbab6ae3d81308353f972e32f1792f
Leonardo Vinicio Diaz Avelar <ldiaza2200@alumno.ipn.mx>
2025-11-05 18:44:22 -0600

    anadiendo archivos

---
+++ entradasalidas/src/main/java/com/hackathon/entradasalidas/EntradasalidasApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.entradasalidas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntradasalidasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntradasalidasApplication.class, args);
+	}
+
+}
---
+++ entradasalidas/src/test/java/com/hackathon/entradasalidas/EntradasalidasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.entradasalidas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntradasalidasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
