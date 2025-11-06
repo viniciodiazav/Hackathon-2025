75acc0127676882816d1a68fc26025b696da31a7
Leonardo Vinicio Diaz Avelar <ldiaza2200@alumno.ipn.mx>
2025-11-05 22:36:29 -0600

    actualizando archivos

--- Interfaz entras y salidas/script.js
+++ Interfaz entras y salidas/script.js
@@ -1,17 +1,38 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const tabs = document.querySelectorAll('.tab-link');
-    const contents = document.querySelectorAll('.content-tab');
 
-    tabs.forEach(tab => {
-        tab.addEventListener('click', () => {
-            const targetId = tab.getAttribute('data-tab');
-            const targetContent = document.getElementById(targetId);
+    const botonNuevaEntrada = document.getElementById('btn-abrir-modal-entrada');
+    const modalEntrada = document.getElementById('modal-entrada');
 
-            tabs.forEach(t => t.classList.remove('active'));
-            contents.forEach(c => c.classList.remove('active'));
+    removerFormularioEntrada();
 
-            tab.classList.add('active');
-            targetContent.classList.add('active');
+    botonNuevaEntrada.addEventListener('click', () => {
+        modalEntrada.classList.add('active');
+    })
+
+    function borrarFormulario() {
+        const proveedorInput = document.getElementById('proveedor');
+        const loteInput = document.getElementById('lote');
+        const modeloInput = document.getElementById('modelo');
+        const tallaInput = document.getElementById('talla');
+        const cantidadInput = document.getElementById('cantidad');
+
+        proveedorInput.value = '';
+        loteInput.value = '';
+        modeloInput.value = '';
+        tallaInput.value = '';
+        cantidadInput.value = '';
+    }
+
+    function removerFormularioEntrada() {
+        modalEntrada.addEventListener('click', e => {
+            if (e.target.id == 'btn-cerrar-modal') {
+                modalEntrada.classList.remove('active');
+                borrarFormulario();
+            } else if (e.target.id == 'modal-entrada') {
+                modalEntrada.classList.remove('active');
+                borrarFormulario();
+            }
         });
-    });
+    }
+
 });
