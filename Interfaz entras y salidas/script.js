d8934edf6672248e4d63c79346083374d6f15a7d
Leonardo Vinicio Diaz Avelar <ldiaza2200@alumno.ipn.mx>
2025-11-05 22:02:59 -0600

    anadiendo interfaz de usuario

---
+++ Interfaz entras y salidas/script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const tabs = document.querySelectorAll('.tab-link');
+    const contents = document.querySelectorAll('.content-tab');
+
+    tabs.forEach(tab => {
+        tab.addEventListener('click', () => {
+            const targetId = tab.getAttribute('data-tab');
+            const targetContent = document.getElementById(targetId);
+
+            tabs.forEach(t => t.classList.remove('active'));
+            contents.forEach(c => c.classList.remove('active'));
+
+            tab.classList.add('active');
+            targetContent.classList.add('active');
+        });
+    });
+});
